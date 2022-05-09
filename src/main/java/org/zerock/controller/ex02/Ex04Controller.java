e43d0eace61c4f3ddeb5b6a3e92dad9ccc028e43
sebaek <sebaek@gmail.com>
2022-05-09 14:12:16 +0900

    omit requestparam

--- src/main/java/org/zerock/controller/ex02/Ex04Controller.java
+++ src/main/java/org/zerock/controller/ex02/Ex04Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("ex04")
 public class Ex04Controller {
-	
+
 	@RequestMapping("sub01")
 	public void method01(HttpServletRequest request) {
 		System.out.println("/ex04/sub01 요청");
@@ -19,44 +19,55 @@ public class Ex04Controller {
 		// 2. bussiness logic 실행
 		// 3. add attribute
 		// 4. forward / redirect
-		
+
 	}
-	
+
 	// @RequestParam 
 	@RequestMapping("sub02")
 	public void method02(@RequestParam("name") String n) {
 		System.out.println(n);
 	}
-	
+
 	@RequestMapping("sub03")
 	public void method03(@RequestParam("name") String n, @RequestParam("age") String age) {
 		System.out.println(n);
 		System.out.println(age);
 		int a = Integer.parseInt(age);
 	}
-	
+
 	@RequestMapping("sub04")
 	public void method04(@RequestParam("age") int age) {
 		System.out.println(age);
 	}
-	
+
 	// url : http://localhost:8080/controller/ex04/sub05?name=sunja&age=88
 	@RequestMapping("sub05")
 	public void method05(HttpServletRequest request) {
 		// 1. request parameter 수집/가공
 		String name = request.getParameter("name");
 		String ageStr = request.getParameter("age");
-		
+
 		int age = Integer.parseInt(ageStr);
-		
+
 		System.out.println(name);
 		System.out.println(age);
 	}
-	
-	
+
+	// url : http://localhost:8080/controller/ex04/sub06?name=sunja&age=88
+	@RequestMapping("sub06")
+	public void method06(@RequestParam String name, @RequestParam int age) {
+		System.out.println(name);
+		System.out.println(age);
+	}
+
+	// url : http://localhost:8080/controller/ex04/sub07?name=sunja&age=88
+	@RequestMapping("sub07")
+	public void method07(String name, int age) {
+		System.out.println(name);
+		System.out.println(age);
+	}
+
 }
-
-
 
 
 
