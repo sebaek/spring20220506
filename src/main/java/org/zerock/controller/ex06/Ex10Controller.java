8a445644198b25f024042f2db554ecdf5f5fa29a
sebaek <sebaek@gmail.com>
2022-05-10 12:30:20 +0900

    redirect attribute

--- src/main/java/org/zerock/controller/ex06/Ex10Controller.java
+++ src/main/java/org/zerock/controller/ex06/Ex10Controller.java
@@ -1,8 +1,10 @@
 package org.zerock.controller.ex06;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.controller.ex03.Customer;
 
 @Controller
 @RequestMapping("ex10")
@@ -19,8 +21,37 @@ public class Ex10Controller {
 	@RequestMapping("sub02")
 	public void method02() {
 		System.out.println("ex10/sub02 경로에서 일함!!!!");
-
+	}
+	
+	@RequestMapping("sub03")
+	public String method03(RedirectAttributes rttr) {
+		rttr.addAttribute("name", "sunja");
+		rttr.addAttribute("age", 99);
+		rttr.addAttribute("address", "seoul");
 		
+		return "redirect:/ex10/sub04";
+	}
+	
+	@RequestMapping("sub04")
+	public void method04() {
+		
+	}
+	
+	@RequestMapping("sub05")
+	public String method05(RedirectAttributes rttr) {
+		Customer c = new Customer();
+		c.setAge(88);
+		c.setName("captain");
+		c.setAddress("ny");
+		
+		rttr.addFlashAttribute("customer", c);
+		
+		return "redirect:/ex10/sub06";
+	}
+	
+	@RequestMapping("sub06")
+	public void method06(Model model) {
+		System.out.println(model.asMap().get("customer"));
 	}
 }
 
